4725b5102ab368bb7cc82f7f92800b46abdf0402
Cristian Ruben <cristian.figueroa.crfe@gmail.com>
2024-04-04 13:57:15 -0600

    delete some unnecesary files

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,34 +1,17 @@
-import { defineConfig } from "astro/config";
-import { remarkReadingTime } from "./remark-reading-time";
-import mdx from "@astrojs/mdx";
-import tailwind from "@astrojs/tailwind";
-import react from "@astrojs/react";
-import remarkToc from "remark-toc";
-import image from "@astrojs/image";
-import prefetch from "@astrojs/prefetch";
-import { SITE } from "./src/config.ts";
+import tailwind from "@astrojs/tailwind"
+import { defineConfig } from "astro/config"
 // https://astro.build/config
+
 export default defineConfig({
-    site: SITE.siteUrl,
-    markdown: {
-        syntaxHighlight: "prism",
-        remarkPlugins: [remarkToc, remarkReadingTime]
+  vite: {
+    optimizeDeps: {
+      exclude: ["@resvg/resvg-js"],
     },
-    vite: {
-        optimizeDeps: {
-            exclude: ["@resvg/resvg-js"]
-        },
-        ssr: {
-            external: ["svgo"]
-        }
+    ssr: {
+      external: ["svgo"],
     },
-    integrations: [
-        mdx(),
-        tailwind(),
-        image({
-            serviceEntryPoint: "@astrojs/image/sharp"
-        }),
-        prefetch(),
-        react()
-    ]
-});
+  },
+  integrations: [
+    tailwind(),
+  ],
+})
--- src/types.ts
+++
@@ -1,6 +0,0 @@
-export type Site = {
-    siteUrl: string;
-    desc: string;
-    title: string;
-    ogImage: string;
-};
